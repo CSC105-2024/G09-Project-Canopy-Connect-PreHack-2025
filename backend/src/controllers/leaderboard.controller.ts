157de2dba336c191499d13c3fe7ec0b00b7de311
Pal <thanapat.doun@mail.kmutt.ac.th>
2025-05-28 17:23:53 +0700

    update

--- backend/src/controllers/leaderboard.controller.ts
+++
@@ -1,8 +0,0 @@
-import type { Context } from 'hono'
-
-import { getLeaderboardModel} from '../models/leaderboard.model.js'
-
-export const getLeaderboard = async (c: Context) => {
-  const leaderboard = await getLeaderboardModel()
-  return c.json(leaderboard)
-}
--- backend/src/routes/leaderboard.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from 'hono'
-import { getLeaderboard } from '../controllers/leaderboard.controller.js'
-
-const leaderboardRoutes = new Hono()
-
-leaderboardRoutes.get('/', getLeaderboard)
-
-export default leaderboardRoutes
