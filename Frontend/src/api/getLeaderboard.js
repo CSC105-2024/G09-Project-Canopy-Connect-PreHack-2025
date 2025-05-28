bb19381637d17751f9fdc44cc12b0979138972c2
Pal <thanapat.doun@mail.kmutt.ac.th>
2025-05-28 17:18:25 +0700

    Delete Frontend directory

--- Frontend/src/api/getLeaderboard.js
+++
@@ -1,9 +0,0 @@
-import { Axios } from "../utils/axiosInstance"
-
-
-const getLeaderboardapi = async () => {
-    const res = await Axios.get('/leaderboard');
-    return res.data;
-}
-
-export {getLeaderboardapi};
